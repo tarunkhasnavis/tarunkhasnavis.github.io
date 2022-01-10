d5f87bd8366453313bc863e1c28424a682d8786c
tarunkhasnavis <khasnavistarun@gmail.com>
2022-01-10 14:30:13 -0800

    changing projects

--- src/Project.js
+++ src/Project.js
@@ -6,14 +6,13 @@ import spotify from './assets/spotify_logo.png';
 import covid19 from './assets/covid-dashboard-icon.png';
 import opyn from './assets/eth-icon.png';
 import pineapple from './assets/pineapple-logo.svg';
-import slack from './assets/slack_logo.png';
+import chainlinklogo from './assets/Chainlink_Logo.png';
 import coingecko from './assets/coingecko_logo.png';
 
 function Project() {
 
     return (
         <div className="projects" id="project"> 
-            {/* <h2>Projects</h2> */}
             <div className="card-container">
                 <ProjectCard 
                     index={0}
@@ -25,44 +24,44 @@ function Project() {
                 />
                 <ProjectCard 
                     index={1}
-                    title="Spotify Lyrics"
-                    description="Spotify Player with Lyrics" 
-                    image={spotify}
-                    link="https://github.com/tarunkhasnavis/spotify-lyrics-player"
-                    git_link="https://github.com/tarunkhasnavis/spotify-lyrics-player"
-                /> 
-                <ProjectCard 
-                    index={2}
-                    title="COVID-19 Dashboard"
-                    description="COVID-19 Stats Visualizer" 
-                    image={covid19}
-                    link="https://github.com/tarunkhasnavis/covid19-dashboard"
-                    git_link="https://github.com/tarunkhasnavis/covid19-dashboard"
-                />
-                <ProjectCard 
-                    index={3}
-                    title="Opyn Dex"
-                    description="Decentralized Excange UI" 
-                    image={opyn}
-                    link="https://opyn-test.web.app/"
-                    git_link="https://github.com/tarunkhasnavis"
-                />
-                <ProjectCard 
-                    index={4}
-                    title="Pineapple Finance"
-                    description="Yeild Optimizer on Etherium" 
-                    image={pineapple}
-                    link="https://github.com/tarunkhasnavis"
-                    git_link="https://github.com/tarunkhasnavis"
-                />
-                <ProjectCard 
-                    index={5}
                     title="Coin Gecko"
                     description="MultiSafe Coin Gecko Clone" 
                     image={coingecko}
                     link="https://multisafe-challenge-a3c7e.web.app/"
                     git_link="https://multisafe-challenge-a3c7e.web.app/"
+                />
+                <ProjectCard 
+                    index={2}
+                    title="Opyn Dex"
+                    description="Decentralized Excange UI" 
+                    image={opyn}
+                    link="https://opyn-test.web.app/"
+                    git_link="https://github.com/tarunkhasnavis"
                 /> 
+                <ProjectCard 
+                    index={3}
+                    title="Multi-Sig Wallet"
+                    description="A Multi-Signature Wallet for Ethereum" 
+                    image={covid19}
+                    link="https://github.com/tarunkhasnavis/multisig-wallet"
+                    git_link="https://github.com/tarunkhasnavis/multisig-wallet"
+                />
+                <ProjectCard 
+                    index={4}
+                    title="Chainlink External Adapter"
+                    description="An API adapter for Chainlink oracles" 
+                    image={chainlinklogo}
+                    link="https://github.com/tarunkhasnavis/external-adapters-js"
+                    git_link="https://github.com/tarunkhasnavis/external-adapters-js"
+                />
+                <ProjectCard 
+                    index={5}
+                    title="Pineapple Finance"
+                    description="Stable Yeild Optimizer on Ethereum" 
+                    image={pineapple}
+                    link="https://github.com/tarunkhasnavis"
+                    git_link="https://github.com/tarunkhasnavis"
+                />
             </div>
         </div>
     )
